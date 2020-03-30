1ade2dbbe010943dd18bad35aac1c6b44b180af8
Nick Groenen <nick.groenen@castoredc.com>
2020-03-30 15:04:17 +0200

    Loop over files in args, don't support stdin

--- src/lib.rs
+++ src/lib.rs
@@ -6,21 +6,16 @@ pub mod wordcounter;
 
 use std::env;
 use std::fs::File;
-use std::io::prelude::*;
 
 use crate::wordcounter::count_words;
 use anyhow::Context;
 
 pub fn run() -> anyhow::Result<()> {
-    let filename = env::args().nth(1).unwrap_or(String::from("-"));
-    let mut f = match filename.as_str() {
-        // https://stackoverflow.com/questions/26378842/how-do-i-overcome-match-arms-with-incompatible-types-for-structs-implementing-sa
-        "-" => Box::new(std::io::stdin()) as Box<dyn Read>,
-        _ => Box::new(File::open(&filename).context(format!("unable to open '{}'", filename))?)
-            as Box<dyn Read>,
-    };
-
-    let wordcount = count_words(&mut f).context("unable to count words")?;
-    println!("{} {}", wordcount, filename);
+    for filename in env::args().skip(1).collect::<Vec<String>>() {
+        let mut reader = File::open(&filename).context(format!("unable to open '{}'", filename))?;
+        let wordcount =
+            count_words(&mut reader).context(format!("unable to count words in '{}'", filename))?;
+        println!("{} {}", wordcount, filename);
+    }
     Ok(())
 }
--- src/wordcounter.rs
+++ src/wordcounter.rs
@@ -23,9 +23,11 @@ pub fn count_words<R: Read>(input: &mut R) -> Result<u32, WordCountError> {
             wordcount += 1;
         }
     }
+
     if wordcount == 0 {
         return Err(WordCountError::EmptySource);
     }
+
     Ok(wordcount)
 }
 
